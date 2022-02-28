8cf08f18f731fa75ce4f03f633af84618fc6b1e3
DESKTOP <DESKTOP@DESKTOP-9U6L7EO>
2022-02-28 21:19:17 +0800

    Add project files.

---
+++ BaetiongActivity2/Controllers/BaetiongController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaetiongActivity2.Controllers
+{
+    public class BaetiongController : Controller
+    {
+        // GET: Baetiong
+        public string MySelf()
+        {
+            return "My Name Is Dunn Lester, I am 20 Years Old and im am enrolled as a BSIT Student";
+        }
+        public string date(int mm, int dd, int yyyy)
+        {
+            return "Today is " + mm + " " + dd + " " + yyyy;
+        }
+        public string me(string name, int age)
+        {
+            return "I am " + name + ". " + "I am " + age + " years old";
+        }
+    }
+}
---
+++ BaetiongActivity2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BaetiongActivity2.MvcApplication" Language="C#" %>
